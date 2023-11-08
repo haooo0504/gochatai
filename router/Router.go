dd3c7a6638387dc24bf81da3042de2545e4dc804
hao <cjunhao0504@gmail.com>
2023-11-08 22:15:50 +0800

    feat: 解決跨域問題 (231108)

--- router/Router.go
+++ router/Router.go
@@ -17,7 +17,7 @@ import (
 // 跨域問題
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", c.GetHeader("Origin"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
